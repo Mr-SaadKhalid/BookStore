da24318c4bd665c7fce114262931b324bdf45b55
Mr-Saadkhalid <saad.khalid@now.net.pk>
2024-06-07 18:36:35 +0500

    build changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KC54T7RS.mjs";import"./chunk-W3WKUBS2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2CKGZKPJ.mjs";import"./chunk-W3WKUBS2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
